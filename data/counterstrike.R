6a17dd97a31d05ced671cd36741520d9d41e3a75
RobinHankin <hankin.robin@gmail.com>
2018-11-03 17:45:23 +1300

    rename variable and add documentation

--- data/counterstrike.R
+++ data/counterstrike.R
@@ -1,4 +1,5 @@
-a <- matrix(c(
+## Data as provided by Zachary:
+counterstrike_kills <- matrix(c(
     0, 4, 4, 6, 0,10, 0, 0, 0, 3, 
     7, 0, 0, 0, 6, 0, 4, 4, 7, 0, 
     9, 0, 0, 0, 5, 0, 7, 5, 9, 0, 
@@ -11,26 +12,38 @@ a <- matrix(c(
     0, 0, 0, 0, 4, 0, 2, 6, 6, 0
 ), 10, 10, byrow=TRUE)
 
-rownames(a) <-
+jj <- 
     c("NiKo","autimatic","tarik","Skadoodle","olofmeister", 
-      "Stewie2k", "karrigan", "GuardiaN","rain","RUSH"      
-      )
-colnames(a) <- rownames(a)
-                 
-team1  <- c("autimatic","tarik", "Skadoodle","Stewie2k","RUSH")
-team2 <- c("NiKo","olofmeister","karrigan","GuardiaN","rain")
+      "Stewie2k", "karrigan", "GuardiaN","rain","RUSH" )
+      
+colnames(counterstrike_kills) <- jj
+rownames(counterstrike_kills) <- jj
 
+## rearrange rows and columns so that rows 1-5 are team 1 and rows
+## 6-10 are team 2:
+## team1: autimatic,tarik, Skadoodle,Stewie2k,RUSH
+## team2: NiKo,olofmeister,karrigan,GuardiaN,rain
 
-dimnames(a) <- list(killer=rownames(a),victim=colnames(a))
+dimnames(counterstrike_kills) <-
+    list(
+        killer = rownames(jj),
+        victim = colnames(jj)
+    )
 
 jj <- c(
     2,3,4,6,10,   # team 1
     1,5,7,8,9     # team 2
 )
 
-a <- a[jj,jj]
+counterstrike_kills <- counterstrike_kills[jj,jj]
 
+## Because shooting members of your own team is forbidden, create
+## structural zeros
 
 jj <- as.matrix(expand.grid(1:5,1:5))
-a[jj,jj] <- NA
-a[jj+5,jj+5] <- NA
+counterstrike_kills[jj  ,jj  ] <- NA
+counterstrike_kills[jj+5,jj+5] <- NA
+
+
+## tidyup:
+rm(jj)
