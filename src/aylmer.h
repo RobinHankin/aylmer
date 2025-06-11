171bf1c3d2805ce9962c288c68c7d7d1d0a366de
RobinHankin <hankin.robin@gmail.com>
2025-06-11 08:29:28 +0100

    indentation

--- src/aylmer.h
+++ src/aylmer.h
@@ -16,88 +16,88 @@ extern "C" {
 // On exit , nval contains the length of the loop (0 means failure to find a loop)
 extern "C" void randpath(int* row, int* nrow, int* col, int* ncol, int* blank, int* nblank, int* ans, int* num) {
   GetRNGstate();
- aylmer::Board board(row,*nrow,col,*ncol,blank,*nblank);
-
-/*
- for(int i=0;i<*nrow;i++) {
- for(int j=0;j<*ncol;j++) if (!ans[i+*nrow*j]) cout << ".\t"; else cout << ans[i+*nrow*j] << "\t"; cout << "\n";
- }
-
- board.print(std::cout);
- */
-
- board.loop(ans,*num);
-
-/*
- cout << "perturbed board:" << endl;
-
- for(int i=0;i<*nrow;i++) {
- for(int j=0;j<*ncol;j++) if (!ans[i+*nrow*j]) cout << ".\t"; else cout << ans[i+*nrow*j] << "\t"; cout << "\n";
- }
- */
+  aylmer::Board board(row,*nrow,col,*ncol,blank,*nblank);
+  
+  /*
+    for(int i=0;i<*nrow;i++) {
+    for(int j=0;j<*ncol;j++) if (!ans[i+*nrow*j]) cout << ".\t"; else cout << ans[i+*nrow*j] << "\t"; cout << "\n";
+    }
+    
+    board.print(std::cout);
+  */
+  
+  board.loop(ans,*num);
+  
+  /*
+    cout << "perturbed board:" << endl;
+    
+    for(int i=0;i<*nrow;i++) {
+    for(int j=0;j<*ncol;j++) if (!ans[i+*nrow*j]) cout << ".\t"; else cout << ans[i+*nrow*j] << "\t"; cout << "\n";
+    }
+  */
   PutRNGstate();
 }
 
 
 extern "C" void numboards(int* row, int* nrow, int* col, int* ncol, int* blank, int* nblank, int* ans) {
- aylmer::     Board  board(row,*nrow,col,*ncol,blank,*nblank);
- aylmer::QuickBoards boards;
-
- boards.go(board,*ans);
-
- *ans=boards.size();
+  aylmer::     Board  board(row,*nrow,col,*ncol,blank,*nblank);
+  aylmer::QuickBoards boards;
+  
+  boards.go(board,*ans);
+  
+  *ans=boards.size();
 }
 
 extern "C" void allboards_c(int* row, int* nrow, int* col, int* ncol, int* blank, int* nblank, int* ans, int* nval) {
- aylmer::     Board  board(row,*nrow,col,*ncol,blank,*nblank);
- aylmer::QuickBoards boards;
-
- int n=*nval;
-
- boards.go(board, *nval);
-
- int i=0;
-
- for(aylmer::Boards::Boards_i iv=boards.begin();n>0;n--,iv++)
- for(int ic=0;ic<*ncol;ic++) 
- for(int ir=0;ir<*nrow;ir++) ans[i++]=iv->first.get_value(ir,ic);
+  aylmer::     Board  board(row,*nrow,col,*ncol,blank,*nblank);
+  aylmer::QuickBoards boards;
+  
+  int n=*nval;
+  
+  boards.go(board, *nval);
+  
+  int i=0;
+  
+  for(aylmer::Boards::Boards_i iv=boards.begin();n>0;n--,iv++)
+    for(int ic=0;ic<*ncol;ic++) 
+      for(int ir=0;ir<*nrow;ir++) ans[i++]=iv->first.get_value(ir,ic);
 }
 
 void randboards(int* row, int* nrow, int* col, int* ncol, int* blank, int* nblank, int   * ans, int* nval  ) {
- GetRNGstate();
- aylmer::      Board  board(row,*nrow,col,*ncol,blank,*nblank);
- aylmer::RandomBoards boards;
-
- boards.go(board,*ans);
-
- int i=0,n=*nval;
-
- for(aylmer::Boards::Boards_i iv=boards.begin();n>0;n--,iv++)
- for(int ic=0;ic<*ncol;ic++) 
- for(int ir=0;ir<*nrow;ir++) ans[i++]=iv->first.get_value(ir,ic);
-
-PutRNGstate();
-
+  GetRNGstate();
+  aylmer::      Board  board(row,*nrow,col,*ncol,blank,*nblank);
+  aylmer::RandomBoards boards;
+  
+  boards.go(board,*ans);
+  
+  int i=0,n=*nval;
+  
+  for(aylmer::Boards::Boards_i iv=boards.begin();n>0;n--,iv++)
+    for(int ic=0;ic<*ncol;ic++) 
+      for(int ir=0;ir<*nrow;ir++) ans[i++]=iv->first.get_value(ir,ic);
+  
+  PutRNGstate();
+  
 }
 
 extern "C" void allboardprobs(int* row, int* nrow, int* col, int* ncol, int* blank, int* nblank, double* ans, int* nval) {
- aylmer::     Board  board(row,*nrow,col,*ncol,blank,*nblank);
- aylmer::QuickBoards boards;
-
- int n=*nval;
-
- boards.go(board,*nval);
-
- int i=0;
-
- for(aylmer::Boards::Boards_i iv=boards.begin();n>0;n--,iv++)
-  ans[i++]=iv->first.get_prob();
+  aylmer::     Board  board(row,*nrow,col,*ncol,blank,*nblank);
+  aylmer::QuickBoards boards;
+  
+  int n=*nval;
+  
+  boards.go(board,*nval);
+  
+  int i=0;
+  
+  for(aylmer::Boards::Boards_i iv=boards.begin();n>0;n--,iv++)
+    ans[i++]=iv->first.get_prob();
 }
 
 void prob(int* row, int* nrow, int* col, int* ncol, int* blank, int* nblank, int* x, double* ans) {
- aylmer::Board board(row,*nrow,col,*ncol,blank,*nblank);
-
- *ans=board.get_prob(x);
+  aylmer::Board board(row,*nrow,col,*ncol,blank,*nblank);
+  
+  *ans=board.get_prob(x);
 }
 
 #endif
